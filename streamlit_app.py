3cccb45bad68e4e020abd52bac6dede1dc29be93
kiso85 <jichu0805@163.com>
2026-03-24 16:55:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -166,7 +166,7 @@ values = [
 
 fig, ax = plt.subplots()
 x = np.array([0, 0.8])   
-width = 0.6
+width = 0.3
 fig, ax = plt.subplots()
 ax.bar(x, values, width=width, color=["#3182bd", "#31a354"])
 
